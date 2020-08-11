2f4dbba1cc220832fd749ebfc318ade2446d904e
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2020-08-11 14:11:33 +0200

    Prepare for release

--- setup.py
+++ setup.py
@@ -23,13 +23,14 @@ def read(name):
 
 
 setup(
-    name="pypicnic",
+    name="python-picnic",
     version="1.0",
     description="Python bindings for picnic",
     long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     author="Sebastian Ramacher",
     author_email="sebastian.ramacher@ait.ac.at",
-    url="https://github.com/sebastinas/pypicnic",
+    url="https://github.com/sebastinas/python-picnic",
     license="Expat",
     ext_modules=cythonize(
         [
@@ -46,16 +47,16 @@ setup(
     ),
     packages=["picnic"],
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Security :: Cryptography",
     ],
     test_suite="picnic.tests",
     setup_requires=["pkgconfig", "cython >= 0.28"],
