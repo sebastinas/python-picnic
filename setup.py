bc6727a754aefca0af12f04d73689bf2d7e4dded
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2021-02-15 14:59:24 +0100

    Use declerative build config as much as possible

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
-import os.path
 import pkgconfig
-import sys
 from setuptools import setup, Extension
-
-
-def read(name):
-    with open(os.path.join(os.path.dirname(__file__), name)) as f:
-        return f.read()
-
 
 if pkgconfig.installed("picnic", ">=3.0.1"):
     flags = pkgconfig.parse("picnic")
@@ -33,29 +25,7 @@ ext_modules = [
 
 
 setup(
-    name="python-picnic",
-    version="1.0",
-    description="Python bindings for picnic",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    author="Sebastian Ramacher",
-    author_email="sebastian.ramacher@ait.ac.at",
-    url="https://github.com/sebastinas/python-picnic",
-    license="Expat",
     ext_modules=ext_modules,
     packages=["picnic"],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Security :: Cryptography",
-    ],
     test_suite="picnic.tests",
-    setup_requires=["pkgconfig", "cython >= 0.28"],
 )
